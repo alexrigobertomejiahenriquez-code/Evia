6164aad8a8da1fd4ef84135ec32c38c7b44ceced
autornocturno1-sudo <324208675+autornocturno1-sudo@users.noreply.github.com>
2026-09-02 19:43:46 -0600

    feat: add search result model

---
+++ lib/screens/search/search_model.dart
@@ -0,0 +1,8 @@
+class SearchResult {
+  final String title;
+  final String subtitle;
+  final String category; // módulo o categoría
+  final String route; // ruta o acción futura
+
+  SearchResult({required this.title, required this.subtitle, required this.category, required this.route});
+}
